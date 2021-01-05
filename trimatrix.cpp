dde145ef305d48852fbb7902139db6b68abeb12a
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2021-01-05 14:55:18 +0100

    WIP: TriMatrix

--- trimatrix.cpp
+++ trimatrix.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <limits>
 #include <gsl/gsl-lite.hpp>
 
@@ -11,6 +12,7 @@ TriMatrix::TriMatrix(ptrdiff_t n)
     if (_n >= 2) {
         gsl_Expects(_n < std::numeric_limits<ptrdiff_t>::max() / (_n - 1));
     }
+
     _diag = std::unique_ptr<double>(new double[_n]);
     _lower = std::unique_ptr<double>(new double[_t]);
     _upper = std::unique_ptr<double>(new double[_t]);
@@ -21,10 +23,23 @@ ptrdiff_t offset_lower_row_major(ptrdiff_t i, ptrdiff_t j) {
     // 2nd summand: offset for j
     return i*(i - 1)/2 + j;
 }
+
 ptrdiff_t offset_upper_row_major(ptrdiff_t i, ptrdiff_t j, ptrdiff_t n) {
     // 1st summand: expansion of n*(n-1)/2 - (n-1-i)*(n-i)/2
     // 2nd summand: offset for j
     return i*(2*n - 1 - i)/2 + (j - i - 1);
+}
+
+ptrdiff_t offset_lower_col_major(ptrdiff_t i, ptrdiff_t j, ptrdiff_t n) {
+    // 1st summand: expansion of n*(n-1)/2 - (n-1-j)*(n-j)/2
+    // 2nd summand: offset for i
+    return j*(2*n - 1 - j)/2 + (i - j - 1);
+}
+
+ptrdiff_t offset_upper_col_major(ptrdiff_t i, ptrdiff_t j) {
+    // 1st summand: sum 0+1+...+(j-1)
+    // 2nd summand: offset for i
+    return j*(j - 1)/2 + i;
 }
 
 double TriMatrix::operator()(ptrdiff_t i, ptrdiff_t j) const
@@ -35,7 +50,7 @@ double TriMatrix::operator()(ptrdiff_t i, ptrdiff_t j) const
     if (i == j) {
         return _diag.get()[i];
     } else if (i > j) { // row-major order
-        return _lower.get()[offset_lower_row_major(i, j)];
+        return _lower.get()[offset_lower_col_major(i, j, _n)];
     } else { // j > i
         return _upper.get()[offset_upper_row_major(i, j, _n)];
     }
@@ -49,7 +64,7 @@ double& TriMatrix::operator()(ptrdiff_t i, ptrdiff_t j)
     if (i == j) {
         return _diag.get()[i];
     } else if (i > j) { // row-major order
-        return _lower.get()[offset_lower_row_major(i, j)];
+        return _lower.get()[offset_lower_col_major(i, j, _n)];
     } else { // j > i
         return _upper.get()[offset_upper_row_major(i, j, _n)];
     }
@@ -57,30 +72,56 @@ double& TriMatrix::operator()(ptrdiff_t i, ptrdiff_t j)
 
 void TriMatrix::scale(double a)
 {
-    for (ptrdiff_t i = 0; i < _n; ++i)
-        _diag.get()[i] *= a;
-    for (ptrdiff_t i = 0; i < _t; ++i)
-        _lower.get()[i] *= a;
-    for (ptrdiff_t i = 0; i < _t; ++i)
-        _upper.get()[i] *= a;
+    double* diag_ptr = _diag.get();
+    for (ptrdiff_t i = 0; i < _n; ++i) {
+        diag_ptr[i] *= a;
+    }
+    
+    double* lower_ptr = _lower.get();
+    for (ptrdiff_t i = 0; i < _t; ++i) {
+        lower_ptr[i] *= a;
+    }
+    
+    double* upper_ptr = _upper.get();
+    for (ptrdiff_t i = 0; i < _t; ++i) {
+        upper_ptr[i] *= a;
+    }
 }
 
 TriMatrix &TriMatrix::operator+=(const TriMatrix &rhs)
 {
     const double* rhs_diag = rhs.diag();
+    double* lhs_diag = diag();
+    for (ptrdiff_t i = 0; i < _n; ++i) {
+        lhs_diag[i] += rhs_diag[i];
+    }
+    
     const double* rhs_lower = rhs.lower();
+    double* lhs_lower = lower();
+    for (ptrdiff_t i = 0; i < _t; ++i) {
+        lhs_lower[i] += rhs_lower[i];
+    }
+    
     const double* rhs_upper = rhs.upper();
-
-    for (ptrdiff_t i = 0; i < _n; ++i)
-        _diag.get()[i] += rhs_diag[i];
-    for (ptrdiff_t i = 0; i < _t; ++i)
-        _lower.get()[i] += rhs_lower[i];
-    for (ptrdiff_t i = 0; i < _t; ++i)
-        _upper.get()[i] += rhs_upper[i];
+    double* lhs_upper = upper();
+    for (ptrdiff_t i = 0; i < _t; ++i) {
+        lhs_upper[i] += rhs_upper[i];
+    }
     return *this;
 }
 
 void TriMatrix::symmetrize()
 {
+    double* diag_ptr = _diag.get();
+    for (ptrdiff_t i = 0; i <_n; ++i) {
+        diag_ptr[i] /= 2.;
+    }
 
+    double* _l = _lower.get();
+    double* _u = _upper.get();
+    for (ptrdiff_t i = 0; i <_t; ++i) {
+        double s = (_l[i] + _u[i]) / 2.;
+        _l[i] = s;
+        _u[i] = s;
+    }
 }
--- trimatrix.h
+++ trimatrix.h
@@ -13,40 +13,36 @@ public:
 
     void scale(double a);
     TriMatrix& operator+=(const TriMatrix& rhs);
+    void transpose() { 
+        _lower.swap(_upper);
+    }
     void symmetrize();
 
-    // TODO: weigh returning std::unique_ptr
-    double* diag() noexcept { return _diag.get(); }
-    double* lower() noexcept { return _lower.get(); };
-    double* upper() noexcept { return _upper.get(); };
-    const double* diag() const noexcept { return _diag.get(); };
-    const double* lower() const noexcept { return _lower.get(); };
-    const double* upper() const noexcept { return _upper.get(); };
-
-    // TODO: Provide iterator interface? (begin/end that iterates
-    // over arrays in succession)
-    // TODO: interface for parallel initialization (each thread initializes part of diag/lower/upper)
-    typedef ptrdiff_t difference_type;
-    typedef size_t size_type;
-    typedef TriMatrix * pointer;
-    typedef TriMatrix & reference;
-
-    class iterator
-    {
-
-    };
-
-    iterator begin() {
-
+    double* diag() noexcept { 
+        return _diag.get();
     }
-
-    iterator end() {
-
+    const double* diag() const noexcept {
+        return _diag.get();
+    }   
+    double* lower() noexcept { 
+        return _lower.get();
+    }
+    const double* lower() const noexcept {
+        return _lower.get();
+    }   
+    double* upper() noexcept { 
+        return _upper.get();
+    }
+    const double* upper() const noexcept { 
+        return _upper.get();
     }
 
     const ptrdiff_t _n;
     const ptrdiff_t _t;
-    ptrdiff_t _s() const noexcept { return _n + 2*_t; };
+
+    ptrdiff_t _s() const noexcept { 
+        return _n + 2*_t;
+    }
 
 private:
     std::unique_ptr<double> _diag;
