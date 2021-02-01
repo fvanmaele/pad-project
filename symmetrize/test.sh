e4f11a41528ca3aba8fcc2056a1d899724fd80ec
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2021-02-01 01:25:51 +0100

    add test output

--- symmetrize/test.sh
+++ symmetrize/test.sh
@@ -4,7 +4,7 @@ set -e
 type g++
 type upcxx-run
 type upcxx
-gpp_flags=(-Wall -Wextra -Wpedantic -g -std=c++17 -march=native)
+gpp_flags=(-Wall -Wextra -Wpedantic -O3 -std=c++17 -march=native)
 
 dims=()
 for k in {5..14}; do
@@ -19,10 +19,6 @@ upcxx "${gpp_flags[@]}" upcxx.cpp -o test-upcxx
 seed=42
 num_repeats=10 # multiple checks (race conditions)
 for n in "${dims[@]}"; do
-    # XXX: Workaround for upcxx.cpp not truncating files
-    #rm 'serial_matrix.txt' 'numa_matrix.txt' 'upcxx_matrix.txt'
-    #rm 'serial_matrix_symmetrized.txt' 'numa_matrix_symmetrized.txt' 'upcxx_matrix.txt'
-
     ./test-serial --dim "$n" --seed="$seed" --write
 
     for i in $(seq 1 "$num_repeats"); do
